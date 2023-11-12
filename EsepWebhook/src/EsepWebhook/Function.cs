b6dc9ce28fc500fc22e6b644c9f5ba9611cba2ca
revav18 <rvijayn@ufl.edu>
2023-11-11 20:02:32 -0500

    added Newton

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,4 +1,7 @@
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using System.Text;
+
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
