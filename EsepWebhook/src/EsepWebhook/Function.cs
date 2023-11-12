cd01628f97de5094f05b6e4a2ca5b43264b4334e
revav18 <rvijayn@ufl.edu>
2023-11-12 11:17:52 -0500

    changed string to object

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,6 +1,7 @@
+using System.Text;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
-using System.Text;
+
 
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -17,14 +18,14 @@ public class Function
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public string FunctionHandler(object input, ILambdaContext context)
     {
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
         
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
         
         var client = new HttpClient();
-        
+
         var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
         {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
